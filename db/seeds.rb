aef1b0be77dc9db68079eaee4cdf8e8c086a632f
anhdtn-1027 <dang.thi.ngoc.anh@sun-asterisk.com>
2020-08-04 13:01:56 +0700

    [manager] show employee

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,15 +26,16 @@ end
 5.times do
   Country.all.each{|country| country.locations.create!(name: Faker::Address.unique.city)}
 end
-
-User.create!(name: "Hoang Anh",
-             email: "nguyen.@gmail.com",
-             password: "hoanganh",
-             password_confirmation: "hoanganh",
-             activated: true,
-             activated_at: Time.zone.now,
-             group_id: 1,
-             role: 1)
+10.times do
+  User.create!(name: Faker::Name.unique.name,
+               email: Faker::Internet.unique.email,
+               password: "123456",
+               password_confirmation: "123456",
+               activated: true,
+               activated_at: Time.zone.now,
+               group_id: Faker::Number.between(1, 5),
+               role: Faker::Number.between(2, 4))
+end
 
 5.times do
   Location.all.each do |location|
@@ -45,4 +46,41 @@ User.create!(name: "Hoang Anh",
       address: address,
       user_id: 1)
   end
+
+5.times do |n|
+  Report.create!(comment: Faker::Lorem.sentence(38),
+                 user_id: 1,
+                 room_id: 1)
+end
+
+Room.all.each do |room|
+  title = Faker::Company.industry
+  description = Faker::Company.catch_phrase
+  message = Faker::Company.catch_phrase
+  user_id = room.user_id
+  start_time = Faker::Time.between(DateTime.now, DateTime.now + 1, :morning)
+  end_time = Faker::Time.between(DateTime.now, DateTime.now + 1, :morning)
+  status = true
+  if (User.find_by(id: user_id).trainee?)
+    status = false
+  end
+  date_event = Faker::Date.forward(23)
+  color = Faker::Number.between(1, 5)
+  if start_time < end_time
+    room.events.create!(
+      title: title,
+      description: description,
+      message: message,
+      user_id: user_id,
+      start_time: start_time,
+      end_time: end_time,
+      status: status,
+      date_event: date_event,
+      color: color
+    )
+  end
+end
+
+Room.all.each do |room|
+  room.update active: :opened
 end
