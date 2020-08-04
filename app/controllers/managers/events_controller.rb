655de5814f07881f5430748a6896744277704cb7
anhdtn-1027 <dang.thi.ngoc.anh@sun-asterisk.com>
2020-08-04 13:01:56 +0700

    [manager] filter events

--- app/controllers/managers/events_controller.rb
+++ app/controllers/managers/events_controller.rb
@@ -1,7 +1,18 @@
 class Managers::EventsController < ManagersController
-  before_action :correct_manager
+  before_action :correct_manager, :get_option
 
   def index
-    @events = Event.page(params[:page]).per Settings.page.size
+    @events = Event.search_events(params[:search], params[:option])
+                   .page(params[:page]).per Settings.page.size
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  private
+
+  def get_option
+    @options = Settings.options.map{|key, val| [t(key.to_s), val]}
   end
 end
