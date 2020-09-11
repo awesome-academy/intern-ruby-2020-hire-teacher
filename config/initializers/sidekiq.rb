58eebb3cd5e05a1096ddec4782b095a72d759af9
manhbnt <nguyen.the.manhb@sun-asterisk.com>
2020-09-11 13:47:41 +0700

    test more

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,15 @@
 require "resolv-replace"
-REDIS_SERVER_CONFIG = Rails.application.config
-  .database_configuration[Rails.env]["redis"]
-REDIS_HOST = REDIS_SERVER_CONFIG["host"]
-REDIS_PORT = REDIS_SERVER_CONFIG["port"]
-REDIS_DB = REDIS_SERVER_CONFIG["db"]
+# REDIS_SERVER_CONFIG = Rails.application.config
+#   .database_configuration[Rails.env]["redis"]
+# REDIS_HOST = REDIS_SERVER_CONFIG["host"]
+# REDIS_PORT = REDIS_SERVER_CONFIG["port"]
+# REDIS_DB = REDIS_SERVER_CONFIG["db"]
 
 Sidekiq::Extensions.enable_delay!
 
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: "redis://#{REDIS_HOST}:#{REDIS_PORT}/12",
+    url: "redis://127.0.0.1:7372/12",
     network_timeout: 5,
     password: "YVr7JafHf7IeZkVsbRjgBadlUnm1tI1LrbB/4RZvQxnWIm4GPB7lPFXoDKhFSzKPfM6R7hrzFj6XwcEv"
   }
@@ -17,7 +17,7 @@ end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: "redis://127.0.0.1:#{REDIS_PORT}/12",
+    url: "redis://127.0.0.1:7372/12",
     network_timeout: 5,
     password: "YVr7JafHf7IeZkVsbRjgBadlUnm1tI1LrbB/4RZvQxnWIm4GPB7lPFXoDKhFSzKPfM6R7hrzFj6XwcEv"
   }
